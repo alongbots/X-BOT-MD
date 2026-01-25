6402bab066cb829eeb759002ca13f9bacd28da84
Along <aosiblings10@gmail.com>
2026-01-25 20:14:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DAVE-S*F=12J35OGICH",
+SESSION_ID: process.env.SESSION_ID || "DAVE-S*F=LX5P2P3S0Y",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -44,7 +44,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "918731810311",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "916009683249",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ALONG",
 // add bot owner name
@@ -83,7 +83,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "918731810311",
+DEV: process.env.DEV || "916009683249",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
